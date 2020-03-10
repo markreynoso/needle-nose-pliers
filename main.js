5e7dc06653a1ee225aaf1ed17565d6fd22d061e5
Mark Reynoso <mreynoso@spu.edu>
2020-03-09 22:04:56 -0700

    ..

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import axios from 'axios';
-
+const Axios = require('axios');
 const Apify = require('apify');
 
 var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
@@ -9,11 +8,12 @@ function updateASI(url) {
     let data = JSON.stringify({
         secondary_app_title: 'test title'
     })
+
     let headers = {
         'Authorization': 'Bearer ' + jwt
     }
 
-    axios.post(url, data, headers)
+    Axios.post(url, data, headers)
         .then(response => {
             console.log(response)
         });
