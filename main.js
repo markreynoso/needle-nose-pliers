56e27ce701f981b4e3ab48be0a001786c450c9ec
Mark Reynoso <mreynoso@spu.edu>
2020-03-09 22:32:49 -0700

    .......

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
 var jwt = process.env.accounts
 
 function updateASI(url) {
-    console.log('url')
-    console.log(url)
     let data = JSON.stringify({
         secondary_app_title: 'test title'
     })
@@ -15,13 +13,12 @@ function updateASI(url) {
         'Authorization': 'Bearer ' + jwt
     }
 
-    axios.post(url, data, headers)
+    axios.patch(url, data, headers)
         .then(response => {
             console.log(response)
         });
 }
 
 Apify.main(async () => {
-    console.log('start')
     updateASI(asi)
 });
