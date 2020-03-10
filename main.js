fd6ce3c2821e1df62e1d0e42af807ca328414ffe
Mark Reynoso <mreynoso@spu.edu>
2020-03-09 23:16:33 -0700

    .......

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
 const axios = require('axios');
 const Apify = require('apify');
 
-var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
-var jwt = process.env.accounts
+// var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
+// var jwt = process.env.accounts
 
-function updateASI(url) {
-    console.log('start')
-    let data = {
-        secondary_app_title: 'test title'
-    }
-    let headers = {
-        Authorization: 'Bearer ' + jwt
-    }
+// function updateASI(url) {
+//     console.log('start')
+//     let data = {
+//         secondary_app_title: 'test title'
+//     }
+//     let headers = {
+//         Authorization: 'Bearer ' + jwt
+//     }
 
-    let res = await axios.patch(url, data, headers)
-        .then(response => {
-            console.log(response)
-        })
-        .catch(err => console.log(err.response))
+//     let res = await axios.patch(url, data, headers)
+//         .then(response => {
+//             console.log(response)
+//         })
+//         .catch(err => console.log(err.response))
     
-    return res
-}
+//     return res
+// }
 
 Apify.main(() => {
     let data = await axios.get('https://apps.apple.com/us/app/id4422025270').then(res => console.log(res))
-    updateASI(asi)
+    console.log(data)
+    return data
+    // updateASI(asi)
 });
