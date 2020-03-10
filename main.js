5255353cf87ccff8652f4318e33947ce03b6f579
Mark Reynoso <mreynoso@spu.edu>
2020-03-09 22:36:28 -0700

    .......

--- main.js
+++ main.js
@@ -5,10 +5,9 @@ var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
 var jwt = process.env.accounts
 
 function updateASI(url) {
-    let data = JSON.stringify({
+    let data = {
         secondary_app_title: 'test title'
-    })
-    console.log(jwt)
+    }
     let headers = {
         'Authorization': 'Bearer ' + jwt
     }
