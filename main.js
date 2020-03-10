d5528e3d57ca5f7bf8b30487ca0d9861cfa04665
Mark Reynoso <mreynoso@spu.edu>
2020-03-09 22:50:01 -0700

    .......

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
 var jwt = process.env.accounts
 
 function updateASI(url) {
+    console.log('start')
     let data = {
         secondary_app_title: 'test title'
     }
@@ -14,7 +15,7 @@ function updateASI(url) {
     console.log(data)
     console.log(headers)
     axios.get(url, headers).then(res => console.log(res))
-    
+
     axios.patch(url, data, headers)
         .then(response => {
             console.log(response)
