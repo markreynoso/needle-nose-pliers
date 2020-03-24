932415185fe0161a1c3b8032803f5a5d15c130eb
Mark Reynoso <mreynoso@spu.edu>
2020-03-23 22:06:38 -0700

    playing with email

--- main.js
+++ main.js
@@ -1,34 +1,4 @@
-const axios = require('axios');
-const Apify = require('apify');
+const Run = require('./app/run.js');
+const Email = require('./app/email.js');
 
-let asi = 'https://core.subsplash.com/accounts/v1/app-store-infos/P5G6Q5'
-let jwt = process.env.accounts
-
-function updateASI(url) {
-    let data = {
-        secondary_app_title: 'test title'
-    }
-    let headers = {
-        Authorization: 'Bearer ' + jwt
-    }
-    
-    axios.patch(url, data, {headers})
-        .then(response => {
-            console.log(response)
-        })
-        .catch(err => console.log(err.response))
-}
-
-function getASI(url) {
-    let headers = {
-        Authorization: 'Bearer ' + jwt
-    }
-    console.log(headers)
-    axios.get(url, {headers})
-        .then(response => {
-            console.log(response)
-        })
-        .catch(err => console.log(err.response))
-}
-getASI(asi)
-updateASI(asi)
+Email.composeEmail('janna.reynoso@gmail.com', 'markdotreynoso@gmail.com', 'I love you!', 'Thanks for my birthday treats!')
